604c0def1b0fabb15293aefb080ab8eae3bba238
Derek Edmond <derekedmond@gmail.com>
2018-11-01 15:20:26 +0000

    add Rectangle.java

---
+++ src/Rectangle.java
@@ -0,0 +1,15 @@
+public class Rectangle {
+
+  int length;
+  int width;
+
+  public Rectangle (int length, int width) {
+    this.length = length;
+    this.width  = width;
+  }
+
+  public getArea() {
+    return length * width;
+  }
+
+}
